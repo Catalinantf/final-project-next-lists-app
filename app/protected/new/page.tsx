d5b65de4f84ed35b356cbfd1835007c881442271
Catalina Tapia <catalinantf@gmail.com>
2024-12-17 16:46:15 -0300

    feat: add Home routes

---
+++ app/protected/new/page.tsx
@@ -0,0 +1,10 @@
+import TodoList from "@/components/ui/home/TodoList/todo-list";
+
+export default function Page() {
+  return (
+    <div>
+      <p>Nuevo Page</p>
+      <TodoList />
+    </div>
+  );
+}
---
+++ app/protected/starred/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <p>Destacados Page</p>;
+}
